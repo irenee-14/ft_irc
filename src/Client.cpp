ec834b9a0a123725181d3c342151c872c251807b
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2024-01-23 19:17:33 +0900

    [Fix] NICK 중복 시 welcome 메시지 안보내는 오류 해결 - checkCmd 함수에서 flag와 저장된 nick, user 확인 후 welcome메시지 보내기
    Co-authored-by: irenee-14 <ireneee145@gmail.com>

--- src/Client.cpp
+++ src/Client.cpp
@@ -3,11 +3,17 @@
 #include <time.h>
 
 #include <iostream>
-Client::Client(void) : _timestamp(time(0)) {}
+Client::Client(void) : _timestamp(time(0)) {
+  _pass_flag = false;
+  _nick_flag = false;
+}
 
-Client::Client(int fd) : _clnt_fd(fd), _timestamp(time(0)) {}
+Client::Client(int fd) : _clnt_fd(fd), _timestamp(time(0)) {
+  _pass_flag = false;
+  _nick_flag = false;
+}
 
-Client::Client(const Client& src) : _timestamp(time(0)) { *this = src; }
+Client::Client(const Client& src) : _timestamp(src._timestamp) { *this = src; }
 
 Client::~Client(void) {}
 
@@ -18,8 +24,9 @@ Client& Client::operator=(Client const& rhs) {
     _user = rhs._user;
     _real_name = rhs._real_name;
     _server_name = rhs._server_name;
-    _pass_flag = rhs._pass_flag;
     _timestamp = rhs._timestamp;
+    _pass_flag = rhs._pass_flag;
+    _nick_flag = rhs._nick_flag;
   }
   return (*this);
 }
@@ -31,8 +38,10 @@ void Client::setRealName(const std::string real) { _real_name = real; }
 void Client::setServerName(const std::string server_name) {
   _server_name = server_name;
 }
-void Client::setPassFlag(const bool flag) { _pass_flag = flag; }
+
 void Client::setTimestamp(const u_int32_t timestamp) { _timestamp = timestamp; }
+void Client::setPassFlag(const bool flag) { _pass_flag = flag; }
+void Client::setNickFlag(const bool flag) { _nick_flag = flag; }
 
 int Client::getFd(void) const { return (_clnt_fd); }
 std::string Client::getUserFd(void) const { return (_user); }
@@ -42,3 +51,4 @@ std::string Client::getRealName(void) const { return (_real_name); }
 std::string Client::getServerName(void) const { return (_server_name); }
 u_int32_t Client::getTimestamp(void) const { return (_timestamp); }
 bool Client::getPassFlag(void) const { return (_pass_flag); }
+bool Client::getNickFlag(void) const { return (_nick_flag); }
