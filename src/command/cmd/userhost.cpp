386330ab90ae0b3bfa3d9e0eb8b4742f085a6a1c
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2024-01-23 19:21:22 +0900

    [Feat] userhost :  인자가 여러개 들어올 때, 각각의 nickname에 맞는 정보 따로 출력
    Co-authored-by: irenee-14 <ireneee145@gmail.com>

--- src/command/cmd/userhost.cpp
+++ src/command/cmd/userhost.cpp
@@ -1,16 +1,30 @@
 #include "Server.hpp"
 
 void Server::userhost(int fd, std::vector<std::string> tokens) {
+  // tokens[1] 에 값이 없을 때
+  // :irc.local 461 root USERHOST :Not enough parameters.
+  if (tokens.size() < 2) {
+    std::string se = ":" + clients[fd].getServerName() + " 461 " +
+                     clients[fd].getNick() +
+                     " USERHOST :Not enough parameters.\r\n";
+    sendString(se, fd);
+    return;
+  }
+
   std::string se = ":" + clients[fd].getServerName() + " 302 " +
                    clients[fd].getNick() + " :";
 
+  // tokens 다 돌면서 nickname에 맞는 정보 출력
+  // userhost root_ root
+  // :irc.local 302 root :root_=+root@127.0.0.1 root=+root@127.0.0.1
   for (unsigned int i = 1; i < tokens.size(); ++i) {
-    se += clients[fd].getNick() + "=+" + clients[fd].getUserFd() + "@" +
-          clients[fd].getServerName() + " ";
+    int user_fd = isUser(tokens[i]);
+    if (user_fd > 0) {
+      se += clients[user_fd].getNick() + "=+" + clients[user_fd].getUserFd() +
+            "@" + clients[user_fd].getServerName() + " ";
+    }
   }
+  // 마지막에 \r\n 붙여서 보내기, 찾는 nickname이 없으면 개행만 보냄
   se += "\r\n";
-
   sendString(se, fd);
 }
-
-// ?? userhost는 자기 정보만 띄워주는 건가 ?? //
