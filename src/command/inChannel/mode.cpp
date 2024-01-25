87ddfd0e2d8cf2b61b09c2a1664ef15d7767a97b
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2024-01-25 17:52:12 +0900

    [Feat] MODE : operator가 아닌 유저가 mode를 변경하려고 할 때 에러 처리
    Co-authored-by: irenee-14 <ireneee145@gmail.com>

--- src/command/inChannel/mode.cpp
+++ src/command/inChannel/mode.cpp
@@ -38,11 +38,13 @@ void Server::mode(int fd, std::vector<std::string> tokens) {
   bool isAddMode = true;
 
   if (channel_idx < 0) return;
+
   // tokens[2] 에 값이 없을 때 채널의 현재 모드 정보 출력
   // !!!!!!!!!!!!!!!!!!!!!!!!!!!
   if (tokens.size() < 3) return;
 
   std::string modeStr = tokens[2];
+
   // tokens에서 3번째부터 끝까지 모드 인자로 받기
   std::vector<std::string> modeArgs =
       std::vector<std::string>(tokens.begin() + 3, tokens.end());
@@ -56,10 +58,27 @@ void Server::mode(int fd, std::vector<std::string> tokens) {
   std::string ret_str;
 
   for (unsigned int i = 0; i < modeStr.size(); ++i) {
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (modeStr[i] == '+')
       isAddMode = true;
     else if (modeStr[i] == '-')
       isAddMode = false;
+    // ----------------------------------------------------------------------
+    // isOperator
+    // 필요한 인자가 다 있고, operator가 아닐 경우
+    // :irc.local 482 root_ #hi :You must have channel op access or above to set
+    // channel mode o
+    std::string un = isAddMode ? "" : "un";
+    if (channels[channel_idx].isOperator(fd) < 0 &&
+        (((modeStr[i] == 'o' || modeStr[i] == 'k') && it != modeArgs.end()) ||
+         (isAddMode && modeStr[i] == 'l' && it != modeArgs.end()))) {
+      std::string se = ":" + SERVER_NAME + " 482 " + clients[fd].getNick() +
+                       " " + channel +
+                       " :You must have channel op access or above to " + un +
+                       "set channel mode " + modeStr[i] + "\r\n";
+      sendString(se, fd);
+      continue;
+    }
     // -------------------------------- "o" --------------------------------
     else if (modeStr[i] == 'o') {
       // *it에 값 없을 경우
@@ -75,13 +94,18 @@ void Server::mode(int fd, std::vector<std::string> tokens) {
       int user_fd = isUser(*it);
       if (user_fd < 0) {
         // 401 ERR_NOSUCHNICK
-        // !!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // :irc.local 401 root nick :No such nick
+        std::string se = ":" + SERVER_NAME + " 401 " + clients[fd].getNick() +
+                         " " + *it + " :No such nick\r\n";
         it++;
         continue;
       }
-      // channel에 user가 존재하지 않거나 이미 operator인 경우 continue
+      // channel에 user가 존재하지 않으면 continue
+      // isAddMode : 이미 operator인 경우 continue
+      // !isAddMode : operator가 아닌 경우 continue
       if (channels[channel_idx].isUser(user_fd) < 0 ||
-          (channels[channel_idx].isOperator(user_fd) >= 0 && isAddMode)) {
+          (channels[channel_idx].isOperator(user_fd) >= 0 && isAddMode) ||
+          (channels[channel_idx].isOperator(user_fd) < 0 && !isAddMode)) {
         it++;
         continue;
       }
@@ -130,7 +154,7 @@ void Server::mode(int fd, std::vector<std::string> tokens) {
     else if (modeStr[i] == 'l') {
       //*it에 값 없을 경우
       // :irc.local 696 root #hi l * :You must specify a parameter for the limit
-      // mode. Syntax: <limit>.å
+      // mode. Syntax: <limit>.
 
       if (isAddMode && it == modeArgs.end()) {
         std::string se = ":" + SERVER_NAME + " 696 " + clients[fd].getNick() +
@@ -157,12 +181,31 @@ void Server::mode(int fd, std::vector<std::string> tokens) {
     }
 
     // -------------------------------- "i" --------------------------------
-    else if (modeStr[i] == 'i')
-      ;
+    else if (modeStr[i] == 'i') {
+      // if ((isAddMode && channels[channel_idx].isInviteOnly()) ||
+      //     (!isAddMode && !channels[channel_idx].isInviteOnly())) {
+      //   continue;
+      // }
+      // if (isAddMode)
+      //   channels[channel_idx].setInviteOnly(true);
+      // else
+      //   channels[channel_idx].setInviteOnly(false);
+      // modes.push_back(t_mode(isAddMode, 'i', ""));
+    }
     // -------------------------------- "t" --------------------------------
-    else if (modeStr[i] == 't')
-      ;
-    else
+    else if (modeStr[i] == 't') {
+      // if ((isAddMode && channels[channel_idx].isTopic()) ||
+      //     (!isAddMode && !channels[channel_idx].isTopic())) {
+      //   continue;
+      // }
+      // if (isAddMode)
+      //   channels[channel_idx].setTopic(true);
+      // else
+      //   channels[channel_idx].setTopic(false);
+      // modes.push_back(t_mode(isAddMode, 't', ""));
+    } else
+      // 없는 모드일 경우 에러 처리
+      // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       ;
     // parsing
     // o, i, t, k, l 일 경우 +, - 따라서 함수 실행
