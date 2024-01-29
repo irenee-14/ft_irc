b7f64d6ff3028b57f648f2b67b8bd17227fd05f8
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2024-01-29 16:32:22 +0900

    [Feat] topic 명령어에 대한 에러 처리 추가
    Co-authored-by: irenee-14 <ireneee145@gmail.com>

--- src/command/channelOper/topic.cpp
+++ src/command/channelOper/topic.cpp
@@ -5,6 +5,12 @@
 
 void Server::topic(int fd, std::vector<std::string> tokens) {
   const std::string channel = tokens[1];
+  if (tokens.size() < 2) {
+    const std::string se = ":" + SERVER_NAME + " 461 " + clients[fd].getNick() +
+                           " TOPIC :Not enough parameters.\r\n";
+    sendString(se, fd);
+    return;
+  }
   const std::string channelNoHash = tokens[1].substr(1, tokens[1].size() - 1);
   const std::string topic = tokens[2];
 
@@ -29,6 +35,14 @@ void Server::topic(int fd, std::vector<std::string> tokens) {
   // 토픽이 설정되어 있는 경우
   // :irc.local 332 root_ #hi :hello
   if (channels[channel_idx].isUser(fd) < 0) {
+    // topic을 변경하려고 할 경우 에러
+    // :irc.local 442 root #asdf :You're not on that channel!
+    if (tokens.size() > 2) {
+      const std::string se = ":" + SERVER_NAME + " 442 " + nickname + " " +
+                             channel + " :You're not on that channel!\r\n";
+      sendString(se, fd);
+      return;
+    }
     const std::string get_topic = channels[channel_idx].getTopic();
     if (get_topic == "") {
       const std::string se = ":" + SERVER_NAME + " 331 " + nickname + " " +
